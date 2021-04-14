4d9dfeb303bda78688fffdeaa9161e59ecc03c3c
Luiz Augusto Machado <luizmachado.to@gmail.com>
2021-04-14 06:33:10 -0300

    Renaming primary key in models

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -2,7 +2,7 @@ namespace minhaflortshirt.Models
 {
     public class Cliente
     {
-        public int id_cliente {get; set;}
+        public int Id {get; set;}
         public string nome_cliente {get; set;}
         public string telefone_cliente {get; set;}
         public string email_cliente {get; set;}
