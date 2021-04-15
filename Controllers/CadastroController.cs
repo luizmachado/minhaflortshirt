e6c237e12a84c6426eb16069329c0e8b72a0859c
Luiz Augusto Machado <luizmachado.to@gmail.com>
2021-04-15 17:09:18 -0300

    In progress

---
+++ Controllers/CadastroController.cs
@@ -0,0 +1,25 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using minhaflortshirt.Models;
+
+namespace minhaflortshirt.Controllers
+{
+    public class CadastroController : Controller
+    {
+       public IActionResult CadastroFornecedor(Fornecedor novoFornecedor)
+       {
+           try
+           {
+               FornecedorRepository fr = new FornecedorRepository();
+               fr.Insert(novoFornecedor);
+
+               return Json(new { Status = "OK"});
+           }
+           catch(Exception e)
+           {
+               return Json(new {Status = "FALHA", Mensagem = e.Message});
+           }
+
+       } 
+    }
+}
