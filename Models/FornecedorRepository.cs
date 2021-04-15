b90caae8128fc519ec2f628ce955235dead270a1
Luiz Augusto Machado <luizmachado.to@gmail.com>
2021-04-15 12:05:03 -0300

    Model Objects Repository was created with Insert and List methods

---
+++ Models/FornecedorRepository.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace minhaflortshirt.Models
+{
+    public class FornecedorRepository
+    {
+        public void Insert(Fornecedor novoFornecedor)
+        {
+            using(var context = new AppDBContext())
+            {
+                context.Fornecedores.Add(novoFornecedor);
+                context.SaveChanges();
+            }
+        }
+
+        public ICollection<Fornecedor> List()
+        {
+            using(var context = new AppDBContext())
+            {
+                return context.Fornecedores.ToList();
+            }
+        }
+    }
+}
