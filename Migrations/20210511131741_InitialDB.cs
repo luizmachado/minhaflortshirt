4f9da4b4af059cba4c434c956daf152277ad86ba
Luiz August Machado <luizmachado.to@gmail.com>
2021-06-17 17:04:05 -0300

    Atualizando migrations

---
+++ Migrations/20210511131741_InitialDB.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MinhaFlorTshirt.Migrations
+{
+    public partial class InitialDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    nome_cliente = table.Column<string>(nullable: true),
+                    telefone_cliente = table.Column<string>(nullable: true),
+                    email_cliente = table.Column<string>(nullable: true),
+                    tamanho_cliente = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Colaboradores",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    nome_colaborador = table.Column<string>(nullable: true),
+                    login_colaborador = table.Column<string>(nullable: true),
+                    senha_colaborador = table.Column<string>(nullable: true),
+                    nascimento_colaborador = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Colaboradores", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Fornecedores",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    nome_fornecedor = table.Column<string>(nullable: true),
+                    telefone_fornecedor = table.Column<string>(nullable: true),
+                    email_fornecedor = table.Column<string>(nullable: true),
+                    site_fornecedor = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fornecedores", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Camisetas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    nome_camiseta = table.Column<string>(nullable: true),
+                    tamanho_camiseta = table.Column<string>(nullable: true),
+                    cor_camiseta = table.Column<string>(nullable: true),
+                    estampa_camiseta = table.Column<string>(nullable: true),
+                    quantidade_camiseta = table.Column<string>(nullable: true),
+                    FornecedorId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Camisetas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Camisetas_Fornecedores_FornecedorId",
+                        column: x => x.FornecedorId,
+                        principalTable: "Fornecedores",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Camisetas_FornecedorId",
+                table: "Camisetas",
+                column: "FornecedorId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Camisetas");
+
+            migrationBuilder.DropTable(
+                name: "Clientes");
+
+            migrationBuilder.DropTable(
+                name: "Colaboradores");
+
+            migrationBuilder.DropTable(
+                name: "Fornecedores");
+        }
+    }
+}
